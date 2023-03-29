c65d6c2d62d02fae166198816af2ff80fd504e32
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2023-03-30 05:24:18 +0600

    complete the framework

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -1,18 +1,60 @@
 defmodule Genetic do
-  @moduledoc """
-  Documentation for `Genetic`.
-  """
+  def initialize(genotype) do
+    for _ <- 1..100, do: genotype.()
+  end
 
-  @doc """
-  Hello world.
+  def evaluate(population, fitness_function) do
+    population
+    |> Enum.sort_by(fitness_function, &>=/2)
+  end
 
-  ## Examples
+  def select(population) do
+    population
+    |> Enum.chunk_every(2)
+    |> Enum.map(&List.to_tuple/1)
+  end
 
-      iex> Genetic.hello()
-      :world
+  def crossover(population) do
+    population
+    |> Enum.reduce([], fn {p1, p2}, acc ->
+      cx_point = :rand.uniform(length(p1))
+      {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
+      {c1, c2} = {h1 ++ t2, h2 ++ t1}
+      [c1, c2 | acc]
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  def mutation(population) do
+    population
+    |> Enum.map(fn chromosome ->
+      if :rand.uniform() < 0.05 do
+        Enum.shuffle(chromosome)
+      else
+        chromosome
+      end
+    end)
+  end
+
+  def run(fitness_function, genotype, max_fitness) do
+    population = initialize(genotype)
+
+    population
+    |> evolve(fitness_function, genotype, max_fitness)
+  end
+
+  def evolve(population, fitness_function, genotype, max_fitness) do
+    population = evaluate(population, fitness_function)
+    best = hd(population)
+    IO.write("\rCurrent Best: #{fitness_function.(best)}")
+
+    if fitness_function.(best) == max_fitness do
+      best
+    else
+      population
+      |> select()
+      |> crossover()
+      |> mutation()
+      |> evolve(fitness_function, genotype, max_fitness)
+    end
   end
 end
