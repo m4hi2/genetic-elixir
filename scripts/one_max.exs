fb8e290fe3b35758e975977829e5994c7e35f9d9
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2023-03-31 02:33:28 +0600

    feat: onemax resolved

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,55 +1,11 @@
-population = for _ <- 1..100, do: for(_ <- 1..1000, do: Enum.random(0..1))
+genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
 
-evaluate = fn population ->
-  Enum.sort_by(population, &Enum.sum/1, &>=/2)
-end
+fitness_function = fn chromosome -> Enum.sum(chromosome) end
 
-selection = fn population ->
-  population
-  |> Enum.chunk_every(2)
-  |> Enum.map(&List.to_tuple(&1))
-end
+max_fitness = 1000
 
-crossover = fn population ->
-  Enum.reduce(population, [], fn {p1, p2}, acc ->
-    cx_point = :rand.uniform(1000)
-    {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
-    [h1 ++ t2, h2 ++ t1 | acc]
-  end)
-end
 
-mutation = fn population ->
-  population
-  |> Enum.map(fn chromosome ->
-    if :rand.uniform() < 0.05 do
-      Enum.shuffle(chromosome)
-    else
-      chromosome
-    end
-  end)
-end
+soln = Genetic.run(fitness_function, genotype, max_fitness)
 
-algorithm = fn population, algorithm ->
-  best = Enum.max_by(population, &Enum.sum/1)
-  IO.write("\r Current Best: " <> Integer.to_string(Enum.sum(best)))
-
-  if Enum.sum(best) == 1000 do
-    best
-  else
-    # Initial Population
-    population
-    # Evaluate Population
-    |> evaluate.()
-    # Selects Parents
-    |> selection.()
-    # Create Children
-    |> crossover.()
-    # Mutate the children
-    |> mutation.()
-    |> algorithm.(algorithm)
-  end
-end
-
-solution = algorithm.(population, algorithm)
-IO.write("\n Answer is \n")
-IO.inspect(solution)
+IO.write("\n")
+IO.inspect(soln)
